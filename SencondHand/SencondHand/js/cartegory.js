842c2abb0f1f5ceab8040114adda7cacd61ee892
yoralong <yoralong@gmail.com>
2022-03-08 10:31:45 +0700

    first

---
+++ SencondHand/SencondHand/js/cartegory.js
@@ -0,0 +1,6 @@
+const itemsliderbar = document.querySelectorAll(".cartegory-left-li");
+itemsliderbar.forEach(function (nemu, index) {
+  menubar.addEventListener("Click", function () {
+    menubar.classList.toggle("block");
+  });
+});
